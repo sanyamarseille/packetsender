b359e202cc49cff616f69dd3837c705951a0c475
root <root@syslog.homelab>
2018-07-28 23:50:56 +0900

    socket_tcp correct

--- main.py
+++ main.py
@@ -1,91 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##### Import module #####
+
+################################
+# import modules
+################################
 import socket
 import time
 import gzip
 import os
 import sys
 from scapy.all import *
-#########################
 
-
-##### Variable settings #####
-
-#### share setting ####
+################################
+# Variable settings
+################################
 start = time.time()
 target_server = ""
-target_port = 
+target_port = 514
 start_path = ""
-i = 0
-### gzip or plain
+i = 1
+
+# gzip / plain
 filemode = "plain"
 
-### scoket or scapy
-mode = "scapy"
+# scoket / scapy
+mode = "socket"
 
-## scapy mode setting
+# scapy mode setting
 source_server = ""
-source_port = 
+source_port = 12345
 
-## socket mode setting
-# tcp or udp
+# socket mode setting
+# tcp / udp
 proto = "tcp"
 flag = False
-#############################
 
-
-
-##### Define action #####
+################################
+# Define action
+################################
 def action(file_path):
-    global i
-
-    #### socket mode
+    global i, flag
+    
+    # socket mode
     if mode == "socket":
-    	### protocol select
 	if proto == "udp":
-    		## UDP
     		client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     	if proto == "tcp":
-		## TCP
     		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #### open and read file 
+    # open and read file 
     if filemode == "gzip":
     	f = gzip.open(file_path,"rb")
     elif filemode == "plain":
 	f = open(file_path,"r")
+
     payload = f.readline()
 
-    #### send log
+    # send data
     while payload:
-	#### socket mode
 	if mode == "socket":
-		## UDP
 		if proto == "udp":
 			client.sendto(payload,(target_server,target_port))
-		## TCP 
-                # connected
 		if proto == "tcp" and flag:
                         client.send(payload)
                 elif proto == "tcp" and flag == False:
         		client.connect((target_server,target_port))
         		client.send(payload)
-                        Flag = True
+                        flag = True
 	
-	#### scapy mode
 	if mode == "scapy":
 		packet = IP(src=source_server, dst=target_server)/UDP(sport=source_port, dport=target_port)/payload
                 send(packet)
-        #### read next line
-        sys.stdout.flush()
-        i = i + 1
-        payload = f.readline()
+        
+	payload = f.readline()
+	i = i + 1
 
-    ######################
 
-    #### file close
+    # file close
     f.close()
 
     if mode == "socket" and proto == "tcp":
@@ -101,10 +93,10 @@ def recursive(path):
     else:
         action(path)
 
-## program start
+################################
+# Main
+################################
 recursive(start_path)
-
 stop = time.time()
-
 print "処理時間：" + str(format(stop-start,'.3f')) + "秒"
 print "処理行数：" + str(i)
